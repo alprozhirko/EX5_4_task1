4a71c0467d284cf0e9748067bbbeee3258394992
ProAlex <alprozhirko@gmail.com>
2023-06-01 22:18:55 +0300

    Initial commit

---
+++ db/src/main/java/DbSetting.java
@@ -0,0 +1,13 @@
+package src.main.java;
+
+public class DbSetting {
+
+    private String name;
+    private String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+}
